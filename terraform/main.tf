b559ebdc77a98c58bd3d54cdf91c8544be904c98
MinhTruong <minhcm2@gmail.com>
2022-09-14 13:53:57 +0700

    add note and modify

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,48 +7,47 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    storage_account_name = "storage206771"
+    container_name       = "mycontainer"
+    key                  = "key1"
+    access_key           = "SypAMAIRddVnqlRFUwl27KWOdPfo99m8Lgi6aM/k76sMW4c0PX+Os/aR+MQxuLJcS6DkSPBcLxX9+AStHUuECA=="
   }
 }
-module "resource_group" {
-  source               = "../../modules/resource_group"
-  resource_group       = "${var.resource_group}"
-  location             = "${var.location}"
-}
+
 module "network" {
-  source               = "../../modules/network"
+  source               = "./modules/network"
   address_space        = "${var.address_space}"
   location             = "${var.location}"
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
-  address_prefix_test  = "${var.address_prefix_test}"
+  resource_group       = var.resource_group_name
+  address_prefix_test  = var.address_prefix_test
 }
 
 module "nsg-test" {
-  source           = "../../modules/networksecuritygroup"
+  source           = "./modules/networksecuritygroup"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = var.resource_group_name
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
+  depends_on = [
+    module.network
+  ]
 }
 module "appservice" {
-  source           = "../../modules/appservice"
+  source           = "./modules/appservice"
   location         = "${var.location}"
-  application_type = "${var.application_type}"
+  application_type = var.application_type
   resource_type    = "AppService"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = "${var.resource_group_name}"
 }
 module "publicip" {
-  source           = "../../modules/publicip"
+  source           = "./modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = var.resource_group_name
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,15 +1,15 @@
 # Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
+subscription_id = "83316a40-5757-40fd-8e78-645eefac51b6"
+client_id = "49374ff0-71c5-4a62-8766-a01190ede0b8"
+client_secret = "YAu8Q~mN5qYSmkf1_DGCugqUf_4-VsUiU3ObkcI2"
+tenant_id = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 
 # Resource Group/Location
-location = ""
-resource_group = ""
-application_type = ""
+location = "East US"
+resource_group_name = "Azuredevops"
+application_type = "webApp"
 
 # Network
-virtual_network_name = ""
+virtual_network_name = "Project-3"
 address_space = ["10.5.0.0/16"]
 address_prefix_test = "10.5.1.0/24"
