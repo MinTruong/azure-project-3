13ae659be210859d5eb96959b19721bc24a09966
MinhTruong <minhcm2@gmail.com>
2022-09-25 21:38:27 +0700

    udate sele

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -5,7 +5,6 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 import logging
 logging.basicConfig(
-    filename="seleniumtestrun.log",
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
